a1084a8f8321ea33fa9d92a4cdd7a81b1dfa664d
Maheshdh1 <dhulipallamahesh02@gmail.com>
2021-11-22 13:59:53 +0000

    Commit Source

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const app = express()
 
 app.listen(3002,() => console.log('Running ..'));
 app.use(express.static("public"));
-app.use(express.json({limit: '1kb'}));
+app.use(express.json({limit: '1mb'}));
 app.post('/api',(request,response) => {
     console.log(request.body);
 })
